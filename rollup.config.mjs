1b83f441aa974ca268e3b070f9978bdf7b599b23
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-10-02 16:30:48 -0500

    fixes to ts generation

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,26 @@
+import typescript from "rollup-plugin-typescript2";
+import dts from "rollup-plugin-dts";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
 
-export default {
-  input: "src/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "es",
-    sourcemap: true,
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [typescript()],
+    external: ["react", "react-dom", "tslib"],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: "./tsconfig.json",
-      useTsconfigDeclarationDir: true,
-    }),
-  ],
-  external: ["react", "react-dom", "tslib"],
-};
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
+    external: ["react", "react-dom", "tslib"],
+  },
+];
