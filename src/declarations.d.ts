a1427761e4fd0200f555a1a157c1830d05a42f73
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-10-02 16:42:22 -0500

    Declare module "un-ods-icons" with exported React.FC components

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,4 +1,19 @@
-declare module "components/*" {
-  const content: any;
-  export default content;
+declare module "un-ods-icons" {
+  export const OdsIcon1: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon2: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon3: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon4: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon5: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon6: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon7: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon8: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon9: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon10: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon11: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon12: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon13: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon14: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon15: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon16: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const OdsIcon17: React.FC<React.SVGProps<SVGSVGElement>>;
 }
